0a6df4677bb3923b50bacb045e7b2a14aca994e4
victors1681 <v.santos@itsoluclick.com>
2020-05-10 17:12:26 -0400

    fixing number array value issue

--- CovidTracker WatchKit Extension/Chart.swift
+++ CovidTracker WatchKit Extension/Chart.swift
@@ -12,12 +12,12 @@ class Graph {
     func chart(states: [StateDaily]?) -> Image? {
         
         guard let cases = states else { return nil }
-        var positives = cases.map{ return $0.positive }
+        var positives = cases.map{ return $0.positive ?? 0 }
         positives.reverse()
         let image = YOBarChartImage()
         image.strokeWidth = 0
         image.fillColor = UIColor(red: 179/255, green: 14/255, blue: 14/255, alpha: 0.7)
-        image.values = positives as! [NSNumber]
+        image.values = positives as [NSNumber]
         let i = image.draw(CGRect(x: 0, y: 0, width: 140, height: 60), scale: 2)
         
         return Image(uiImage: i)
